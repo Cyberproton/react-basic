600c6e1ed9a6b0751cea9151ce09deb0dca71bcc
BuildTools <unconfigured@null.spigotmc.org>
2020-07-16 21:03:02 +0700

    Add sidedrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,13 @@
 import React, { Fragment } from 'react';
 
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 import styles from './Layout.module.css';
 
 const Layout = (props) => (
     <Fragment>
         <Toolbar />
+        <SideDrawer />
         <main className={styles.Content}>
             {props.children}
         </main>
---
+++ src/components/Navigation/Sidedrawer/SideDrawer.js
@@ -0,0 +1,18 @@
+import React from './node_modules/react';
+
+import NavigationItems from '../NavigationItems/NavigationItems';
+import Logo from '../../Logo/Logo';
+import styles from './SideDrawer.module.css';
+
+const SideDrawer = () => {
+    return (
+        <div className={styles.SideDrawer}>
+            <Logo />
+            <nav>
+                <NavigationItems />
+            </nav>
+        </div>
+    );
+};
+
+export default SideDrawer;
