4ccd91aaf6d7a23e7dd8dddbbf135c1f66e74a28
Cyberproton <cyberprotonatmailbox@gmail.com>
2020-07-27 19:13:06 +0700

    Add loading spinner

---
+++ src/components/UI/Spinner/Spinner.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import styles from './Spinner.module.css';
+
+const spinner = () => {
+    return <div className={styles.Loader}>Please wait</div>
+};
+
+export default spinner;
